f4d2246d869122262df4265158966518c677f966
Štěpánka Pokorná <steankapokorna1@seznam.cz>
2025-12-19 18:39:44 +0100

    Dokončení hamburger menu

--- spolecne.js
+++ spolecne.js
@@ -5,5 +5,12 @@ const menuItemsElm = document.querySelector('#menu-polozky');
 
 menuButtonElm.addEventListener('click', () => {
     menuItemsElm.classList.toggle('show');
+
+    if (menuItemsElm.classList.contains('show')){
+        menuButtonElm.innerHTML = '<i class="fas fa-xmark"></i>'
+    } else{
+        menuButtonElm.innerHTML = '<i class="fas fa-bars"></i>'
+    }
+     
 });
 
